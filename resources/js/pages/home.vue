5818c9b88a39f467e19a3421fb5881f7946b3105
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-08-26 23:42:10 +0300

    Starting Laravel vue SPA

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col-lg-10 m-auto">
+      <card :title="$t('home')">
+        {{ $t('you_are_logged_in') }}
+        <fetch-users />
+      </card>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import FetchUsers from '~/components/FetchUsers'
+export default {
+  components: {
+    FetchUsers
+  },
+  middleware: 'auth',
+
+  // async asyncData () {
+  //   const { data: projects } = await axios.get('/api/projects')
+
+  //   return {
+  //     projects
+  //   }
+  // },
+
+  metaInfo () {
+    return { title: this.$t('home') }
+  }
+}
+</script>
